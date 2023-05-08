de5b9b758889bcfb1d3267db7222a547c4ab3f19
LinL <572727657@qq.com>
2023-05-08 09:32:48 +0800

    merge

--- labels/labelUtils.py
+++
@@ -1,8 +0,0 @@
-names = []
-labels = open('../labels/labels.txt').readlines()
-for label in labels:
-    names.append(label)
-output = ''
-for name in names:
-    output += '"' + name.replace('\n', '') + '"' + ','
-print(output)
